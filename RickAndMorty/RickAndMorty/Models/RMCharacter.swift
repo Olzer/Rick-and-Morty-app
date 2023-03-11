5766649aaef1c726977824b85742b0d59113df91
Oleh Dorozhovers <olzer123@gmail.com>
2023-03-11 15:27:43 +0100

    Created project and added a folder for future project structure. Created NavigationBarController and added 4 View Controllers for each category: 'Character', 'Location', 'Episode', and 'Settings'. Refactored 'setUpTabs' function.

---
+++ RickAndMorty/RickAndMorty/Models/RMCharacter.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacter.swift
+//  RickAndMorty
+//
+//  Created by Oleg Dorozhovets on 11/03/2023.
+//
+
+import Foundation
---
+++ RickAndMorty/RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  RickAndMorty
+//
+//  Created by Oleg Dorozhovets on 11/03/2023.
+//
+
+import Foundation
